0746beff84255c10a60e7e0be83ae601d56044b9
klchen0112 <klchen0112@gmail.com>
2024-01-12 21:46:06 +0800

    add TsangerJinKai02 Jigmo emacs-plus

--- flake.nix
+++ flake.nix
@@ -1,7 +1,25 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; # Nix Packages
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; # Nix Packages
+
+    emacs-overlay = {
+        url = "github:nix-community/emacs-overlay/master";
+        inputs.nixpkgs.follows = "nixpkgs-unstable";
+        inputs.nixpkgs-stable.follows = "nixpkgs";
+        inputs.flake-utils.follows = "flake-utils";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+
+    systems.url = "github:nix-systems/default";
+
+  };
+  outputs = inputs @ { self, nixpkgs,... }:
     let
       systems = [
         "x86_64-linux"
@@ -14,9 +32,10 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      legacyPackages = forAllSystems (system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      legacyPackages = forAllSystems (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        import ./default.nix {inherit pkgs inputs;});
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
