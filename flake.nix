790d0405bcbd1ca1534d9f780274355ad8578d2b
klchen0112 <klchen0112@gmail.com>
2024-01-12 22:13:05 +0800

    change packages

--- flake.nix
+++ flake.nix
@@ -2,22 +2,6 @@
   description = "My personal NUR repository";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; # Nix Packages
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable"; # Nix Packages
-
-    emacs-overlay = {
-        url = "github:nix-community/emacs-overlay/master";
-        inputs.nixpkgs.follows = "nixpkgs-unstable";
-        inputs.nixpkgs-stable.follows = "nixpkgs";
-        inputs.flake-utils.follows = "flake-utils";
-    };
-
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-
-    systems.url = "github:nix-systems/default";
-
   };
   outputs = inputs @ { self, nixpkgs,... }:
     let
@@ -35,7 +19,7 @@
       legacyPackages = forAllSystems (system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in
-        import ./default.nix {inherit pkgs inputs;});
+        import ./default.nix {inherit pkgs;});
       packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
     };
 }
